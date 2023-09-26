9bbe8890f0f024ac3e942f535734e8b0888479e1
Calvin Armstrong <138508650+armstrca@users.noreply.github.com>
2023-09-26 17:01:06 +0000

    update

---
+++ app/controllers/mathtime_controller.rb
@@ -0,0 +1,53 @@
+class MathtimeController < ApplicationController
+
+  def home
+    render({ :template => "layouts/square" })
+  end
+
+  def square
+    render({ :template => "layouts/square" })
+  end
+
+  def square_results
+    @number = params.fetch("number")
+    @num_sq = (@number.to_f)**2
+    render({ :template => "layouts/square_results" })
+  end
+
+  def square_root
+    render({ :template => "layouts/sqrt" })
+  end
+
+  def square_root_results
+    @number = params.fetch("number")
+    @num_sqrt = (@number.to_f)**0.5
+    render({ :template => "layouts/sqrt_results" })
+  end
+
+  def payment
+    render({ :template => "layouts/payment" })
+  end
+
+  def payment_results
+    @apr = ((params.fetch("apr")).to_f).round(4)
+    @years = params.fetch("years")
+    @months = (@years.to_i * 12)
+    @aprmonthly = @apr/12
+    @principal = (params.fetch("principal")).to_f
+    @payment_num = ((@aprmonthly/100) * @principal)
+    @payment_denom = (1 - ((1 + (@aprmonthly/100)))**(-@months))
+    @payment = ((@payment_num.to_f)/(@payment_denom.to_f)).to_fs(:currency)
+    render({ :template => "layouts/payment_results" })
+  end
+
+  def random
+    render({ :template => "layouts/random" })
+  end
+
+  def random_results
+    @min = params.fetch("min").to_f
+    @max = params.fetch("max").to_f
+    @output = rand(@min..@max)
+    render({ :template => "layouts/random_results" })
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  
+  get("/square", { :controller => "mathtime", :action => "square" })
+
+  get("square_root", { :controller => "mathtime", :action => "square_root" })
+
+  get("/random", { :controller => "mathtime", :action => "random" })
+
+  get("/", { :controller => "mathtime", :action => "square" })
+
 end
